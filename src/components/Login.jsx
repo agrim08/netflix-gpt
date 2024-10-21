2b85b063373da00f74d98381ba89e0e51d655f99
agrim08 <agrimgupta0805@gmail.com>
2024-10-21 19:55:11 +0530

    form validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import Header from "./Header";
 import { useState } from "react";
+import { checkValidation } from "../utils/validate";
 
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleFormData = () => {
+    //* validate form data
+    const message = checkValidation(email.current.value, email.current.value);
+    setErrorMessage(message);
+    console.log(message);
+  };
 
   const toggleSignInForm = () => {
     setIsSignIn(!isSignIn);
   };
+
   return (
     <div>
       <Header />
@@ -18,11 +31,14 @@ const Login = () => {
           alt="bg-image"
         />
       </div>
-      <form className="absolute p-12 bg-[rgba(0,0,0,0.80)] w-2/5 h-full my-auto top-24 bottom-0 mx-auto right-0 left-0 flex flex-col items-center rounded-md ">
+      <form
+        className="absolute p-12 bg-[rgba(0,0,0,0.80)] w-2/5 h-full my-auto top-24 bottom-0 mx-auto right-0 left-0 flex flex-col items-center rounded-md "
+        onSubmit={(e) => e.preventDefault()}
+      >
         <h1 className="text-white mb-6 text-3xl font-bold w-4/5">
           {isSignIn ? "Sign In" : "Sign Up"}
         </h1>
-        {isSignIn ? (
+        {!isSignIn ? (
           <input
             type="text"
             placeholder="Username"
@@ -30,16 +46,22 @@ const Login = () => {
           />
         ) : null}
         <input
+          ref={email}
           type="text"
           placeholder="Email or mobile number"
           className="p-2 m-2 bg-[#000438] text-white w-4/5 h-16 border border-white rounded-md opacity-60"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-2 m-2 bg-[#000438] text-white w-4/5 h-16 border border-white rounded-md opacity-60"
         />
-        <button className="p-4 m-4 bg-red-600 text-white w-4/5 h-10 py-2 rounded-sm">
+        <p className="text-red-600 font-medium text-xl">{errorMessage}</p>
+        <button
+          className="p-4 m-4 bg-red-600 text-white w-4/5 h-10 py-2 rounded-sm"
+          onClick={handleFormData}
+        >
           {isSignIn ? "Sign In" : "Sign up"}
         </button>
         <span className="text-white text-lg">OR</span>
